953894198bfd45e742e8e6d6e5323c5c64003cb3
r74tech <r74tech@gmail.com>
2025-07-19 12:46:47 +0900

    Update shebang to use 'bun' instead of 'node' in cli.ts and index.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import { parseArgs } from "node:util";
 import chalk from "chalk";
 import { commands } from "./commands/commands.ts";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import { cli } from "./cli.ts";
 
 // Export function matching filename
